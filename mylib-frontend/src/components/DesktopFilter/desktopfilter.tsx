bf8bb71af7fe33aab0aac3c9c96aa4173f908e37
Gregory Stathis <gstathis22@gmail.com>
2023-04-14 13:14:50 -0400

    Front end change
    - Factored out common component that both filters used

--- mylib-frontend/src/components/DesktopFilter/desktopfilter.tsx
+++ mylib-frontend/src/components/DesktopFilter/desktopfilter.tsx
@@ -1,9 +1,8 @@
-import { useState, useRef, useEffect } from "react"
-import { FaFilter } from "react-icons/fa"
 import FilterSelect from "../FilterSelect/filterselect"
 import FilterDate from "../FilterDate/filterdate"
 import FilterDropdownItem from "../FilterDropdownItem/filterdropdownitem"
 import styles from "./desktopfilter.module.css"
+import DropdownContainer from "../DropdownContainer/dropdowncontainer"
 
 type DesktopFilterProps = {
     readStatusFilter: string,
@@ -16,40 +15,11 @@ type DesktopFilterProps = {
 }
 
 export default function DesktopFilter({readStatusFilter, searchQuery, dateCompletedFilter, setSearchQuery, setReadStatusFilter, setDateCompletedFilter, clearFilters}: DesktopFilterProps) {
-    const dropDownRef = useRef<any>(null)
-
-    const [showFilterDropdown, setShowFilterDropdown] = useState(styles.hideFilterDropdown)
-
-    useEffect(() => {
-        function handleClickOutside(event: any) {
-            if (dropDownRef.current && !dropDownRef.current.contains(event.target) && showFilterDropdown === styles.filterDropdown) {
-                setShowFilterDropdown(styles.hideFilterDropdown)
-            }
-        }
-
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
-
-    }, [dropDownRef, showFilterDropdown])
-
-    function openCloseFilterDropdown() {
-        if(showFilterDropdown === styles.hideFilterDropdown) {
-            setShowFilterDropdown(styles.filterDropdown)
-        } else {
-            setShowFilterDropdown(styles.hideFilterDropdown)
-        }
-    }
 
     return (
         <>
             <input type="search" className={styles.searchBar} placeholder="Look up a title or author" size={30} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-            <div ref={dropDownRef} className={styles.filterDropdownContainer}>
-                <button onClick={() => openCloseFilterDropdown()} className={styles.filterButton}><FaFilter/></button>
-                <div className={showFilterDropdown}>
+                <DropdownContainer dropdownStyle={styles.filterDropdown}>
                     <FilterDropdownItem filterName="Status">
                         <FilterSelect 
                             listOfSelectValues={["Not Read", "Reading", "Completed"]}
@@ -64,9 +34,7 @@ export default function DesktopFilter({readStatusFilter, searchQuery, dateComple
                             setSelectValue={setDateCompletedFilter}
                         />
                     </FilterDropdownItem>
-
-                </div>
-            </div>
+                </DropdownContainer>
             <button onClick={() => clearFilters()} className={styles.clearFilterButton}>clear filter</button>
         </>
     )
---
+++ mylib-frontend/src/components/DropdownContainer/dropdowncontainer.tsx
@@ -0,0 +1,48 @@
+import { PropsWithChildren, useRef, useEffect, useState } from "react";
+import { FaFilter } from "react-icons/fa"
+import styles from "./dropdowncontainer.module.css"
+
+type DropdownContainerProps = PropsWithChildren & {
+    dropdownStyle: string
+
+}
+
+export default function DropdownContainer({children, dropdownStyle}: DropdownContainerProps) {
+
+    const dropDownRef = useRef<any>(null)
+
+    const [showFilterDropdown, setShowFilterDropdown] = useState(styles.hideFilterDropdown)
+
+    useEffect(() => {
+        function handleClickOutside(event: any) {
+            if (dropDownRef.current && !dropDownRef.current.contains(event.target) && showFilterDropdown === dropdownStyle) {
+                setShowFilterDropdown(styles.hideFilterDropdown)
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+    }, [dropDownRef, showFilterDropdown])
+
+    function openCloseFilterDropdown() {
+        if(showFilterDropdown === styles.hideFilterDropdown) {
+            setShowFilterDropdown(dropdownStyle)
+        } else {
+            setShowFilterDropdown(styles.hideFilterDropdown)
+        }
+    }
+
+    return (
+        <div ref={dropDownRef} className={styles.filterDropdownContainer}>
+            <button onClick={() => openCloseFilterDropdown()} className={styles.filterButton}><FaFilter/></button>
+            <div className={showFilterDropdown}>
+                {children}
+            </div>
+        </div>
+    )
+}
